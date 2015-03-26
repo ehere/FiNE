b11572e8954c8b8db9f4110157df4a9dbd59a009
tanasab <tanasab2@gmail.com>
2015-03-26 12:50:57 +0700

    Add Servlet Context Listener And Change contextpath
    Add db connection in listener
    Update how to get Project path

--- src/java/help/F.java
+++ src/java/help/F.java
@@ -12,11 +12,19 @@ import java.io.Serializable;
  * @author tanasab
  */
 public class F implements Serializable{
+    private static String projectPath;
+    
     public static String asset(String url){
-        String path = F.class.getResource("").getPath();
-        path = path.replace("build/web/WEB-INF/classes/help/", "").replaceAll("^/+", "").replaceAll("/+$", "");
-        path = path.substring(path.lastIndexOf('/') + 1);
-        return "/"+path+"/"+url.replaceAll("^/+", "").replaceAll("/+$", "");
+
+        return projectPath+"/"+url.replaceAll("^/+", "").replaceAll("/+$", "");
+    }
+
+    public static String getProjectPath() {
+        return projectPath;
+    }
+
+    public static void setProjectPath(String projectPath) {
+        F.projectPath = projectPath;
     }
     
 }
